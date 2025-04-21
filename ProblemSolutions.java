c0761fb9f328f36296da4ede732be60a5fbb64b4
ben flowers <149430451+jalenbenny@users.noreply.github.com>
2025-04-21 16:43:47 -0500

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   ADD YOUR NAME / SECTION NUMBER HERE
+ *   BEN FLOWERS / COMP 272/400C 002
  *
  *   This java file contains the problem solutions of canFinish and
  *   numGroups methods.
@@ -18,73 +18,61 @@ class ProblemSolutions {
      * exams certifications that must be taken by programmers in order to obtain
      * a new certification called 'master programmer'. In doing so, you are placing
      * prerequisites on intermediate exam certifications that must be taken first.
-     * You are allowing the flexibility of taking the exams in any order as long as
-     * any exam prerequisites are satisfied.
-     *
-     * Unfortunately, in the past, your predecessors have accidentally published
-     * curriculums and exam schedules that were not possible to complete due to cycles
-     * in prerequisites. You want to avoid this embarrassment by making sure you define
-     * a curriculum and exam schedule that can be completed.
-     *
-     * You goal is to ensure that any student pursuing the certificate of 'master
-     * programmer', can complete 'n' certification exams, each being specific to a
-     * topic. Some exams have prerequisites of needing to take and pass earlier
-     * certificate exams. You do not want to force any order of taking the exams, but
-     * you want to make sure that at least one order is possible.
-     *
-     * This method will save your embarrassment by returning true or false if
-     * there is at least one order that can taken of exams.
-     *
-     * You wrote this method, and in doing so, you represent these 'n' exams as
-     * nodes in a graph, numbered from 0 to n-1. And you represent the prerequisite
-     * between taking exams as directed edges between two nodes which represent
-     * those two exams.
      *
      * Your method expects a 2-dimensional array of exam prerequisites, where
      * prerequisites[i] = [ai, bi] indicating that you must take exam 'bi' first
-     * if you want to take exam 'ai'. For example, the pair [0, 1], indicates that
-     * to take exam certification '0', you have to first have the certification for
-     * exam '1'.
+     * if you want to take exam 'ai'.
      *
      * The method will return true if you can finish all certification exams.
      * Otherwise, return false (e.g., meaning it is a cyclic or cycle graph).
      *
-     *     Example 1:
-     *     Input: numExams = 2, prerequisites = [[1,0]]
-     *     Output: true
-     *     Explanation: There are a total of 2 exams to take.
-     *     To take exam 1 you should have completed the
-     *     certification of exam 0. So, it is possible (no
-     *     cyclic or cycle graph of prereqs).
-     *
-     *
-     *     Example 2:
-     *     Input: numExams = 2, prerequisites = [[1,0],[0,1]]
-     *     Output: false
-     *     Explanation: There are a total of 2 exams to take.
-     *     To take exam 1 you should have completed the
-     *     certification of exam 0, and to take exams 0 you
-     *     should also have completed the certification of exam
-     *     1. So, it is impossible (it is a cycle graph).
-     *
-     * @param numExams          - number of exams, which will produce a graph of n nodes
-     * @param prerequisites     - 2-dim array of directed edges.
-     * @return boolean          - True if all exams can be taken, else false.
+     * @param numExams      - number of exams, which will produce a graph of n nodes
+     * @param prerequisites - 2-dim array of directed edges.
+     * @return boolean      - True if all exams can be taken, else false.
      */
+    public boolean canFinish(int numExams, int[][] prerequisites) {
 
-    public boolean canFinish(int numExams, 
-                             int[][] prerequisites) {
-      
-        int numNodes = numExams;  // # of nodes in graph
+        // build graphand arrau
+        ArrayList<Integer>[] adj = new ArrayList[numExams];
+        int[] preReqCount = new int[numExams];
 
-        // Build directed graph's adjacency list
-        ArrayList<Integer>[] adj = getAdjList(numExams, 
-                                        prerequisites); 
+        for (int i = 0; i < numExams; i++) {
+            adj[i] = new ArrayList<>();
+        }
 
-        // ADD YOUR CODE HERE - ADD YOUR NAME / SECTION AT TOP OF FILE
-        return false;
+        for (int[] prereq : prerequisites) {
+            int to = prereq[0];
+            int from = prereq[1];
+            adj[from].add(to);
+            preReqCount[to]++;
+        }
 
+        // add all nodes with prereq count 0 to queue
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numExams; i++) {
+            if (preReqCount[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        // processing nodes with bfs
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            count++;
+
+            for (int neighbor : adj[curr]) {
+                preReqCount[neighbor]--;
+                if (preReqCount[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        // valid schedule if all exams are processed
+        return count == numExams;
     }
+
 
 
     /**
@@ -93,106 +81,78 @@ class ProblemSolutions {
      * Building an Adjacency List for the directed graph based on number of nodes
      * and passed in directed edges.
      *
-     * @param numNodes      - number of nodes in graph (labeled 0 through n-1) for n nodes
-     * @param edges         - 2-dim array of directed edges
-     * @return ArrayList<Integer>[]  - An adjacency list representing the provided graph.
+     * @param numNodes - number of nodes in graph (labeled 0 through n-1)
+     * @param edges    - 2-dim array of directed edges
+     * @return ArrayList<Integer>[] - An adjacency list representing the graph
      */
-
-    private ArrayList<Integer>[] getAdjList(
-            int numNodes, int[][] edges) {
-
-        ArrayList<Integer>[] adj 
-                    = new ArrayList[numNodes];      // Create an array of ArrayList ADT
-
-        for (int node = 0; node < numNodes; node++){
-            adj[node] = new ArrayList<Integer>();   // Allocate empty ArrayList per node
+    private ArrayList<Integer>[] getAdjList(int numNodes, int[][] edges) {
+        ArrayList<Integer>[] adj = new ArrayList[numNodes];
+        for (int node = 0; node < numNodes; node++) {
+            adj[node] = new ArrayList<>();
         }
-        for (int[] edge : edges){
-            adj[edge[0]].add(edge[1]);              // Add connected node edge [1] for node [0]
+        for (int[] edge : edges) {
+            adj[edge[0]].add(edge[1]);
         }
         return adj;
     }
 
-
-    /*
-     * Assignment Graphing - Number of groups.
+    /**
+     * Method numGroups
      *
-     * There are n people. Some of them are connected
-     * as friends forming a group. If person 'a' is
-     * connected to person 'b', and person 'b' is
-     * connected to person 'c', they form a connected
-     * group.
+     * Given an undirected adjacency matrix representing connections between people,
+     * return the number of disconnected groups (connected components) in the graph.
      *
-     * Not all groups are interconnected, meaning there
-     * can be 1 or more groups depending on how people
-     * are connected.
-     *
-     * This example can be viewed as a graph problem,
-     * where people are represented as nodes, and
-     * edges between them represent people being
-     * connected. In this problem, we are representing
-     * this graph externally as an non-directed
-     * Adjacency Matrix. And the graph itself may not
-     * be fully connected, it can have 1 or more
-     * non-connected compoents (subgraphs).
-     *
-     * Example 1:
-     *   Input :
-         AdjMatrix = [[0,1,0], [1,0,0], [0,0,0]]
-     *   Output: 2
-     *   Explanation: The Adjacency Matrix defines an
-     *   undirected graph of 3 nodes (indexed 0 to 2).
-     *   Where nodes 0 and 1 aee connected, and node 2
-     *   is NOT connected. This forms two groups of
-     *   nodes.
-     *
-     * Example 2:
-     *   Input : AdjMatrix = [ [0,0,0], [0,0,0], [0,0,0]]
-     *   Output: 3
-     *   Explanation: The Adjacency Matrix defines an
-     *   undirected graph of 3 nodes (indexed 0 to 2).
-     *   There are no connected nodes, hence forming
-     *   three groups.
-     *
-     * Example 3:
-     *   Input : AdjMatrix = [[0,1,0], [1,0,0], [0,1,0]]
-     *   Output: 1
-     *   Explanation, The adjacency Matrix defined an
-     *   undirected graph of 3 nodes (index 0 to 2).
-     *   All three nodes are connected by at least one
-     *   edge. So they form on large group.
+     * @param adjMatrix - adjacency matrix representing graph connections
+     * @return int - number of groups (connected components)
      */
-
     public int numGroups(int[][] adjMatrix) {
         int numNodes = adjMatrix.length;
-        Map<Integer,List<Integer>> graph = new HashMap();
-        int i = 0, j =0;
+        Map<Integer, List<Integer>> graph = new HashMap<>();
 
-        /*
-         * Converting the Graph Adjacency Matrix to
-         * an Adjacency List representation. This
-         * sample code illustrates a technique to do so.
-         */
-
-        for(i = 0; i < numNodes ; i++){
-            for(j = 0; j < numNodes; j++){
-                if( adjMatrix[i][j] == 1 && i != j ){
-                    // Add AdjList for node i if not there
-                    graph.putIfAbsent(i, new ArrayList());
-                    // Add AdjList for node j if not there
-                    graph.putIfAbsent(j, new ArrayList());
-
-                    // Update node i adjList to include node j
+        for (int i = 0; i < numNodes; i++) {
+            for (int j = 0; j < numNodes; j++) {
+                if (adjMatrix[i][j] == 1 && i != j) {
+                    graph.putIfAbsent(i, new ArrayList<>());
+                    graph.putIfAbsent(j, new ArrayList<>());
                     graph.get(i).add(j);
-                    // Update node j adjList to include node i
                     graph.get(j).add(i);
                 }
             }
         }
 
-        // YOUR CODE GOES HERE - you can add helper methods, you do not need
-        // to put all code in this method.
-        return -1;
+        boolean[] visited = new boolean[numNodes];
+        int groups = 0;
+
+        for (int i = 0; i < numNodes; i++) {
+            if (!visited[i]) {
+                dfs(i, graph, visited);
+                groups++;
+            }
+        }
+
+        return groups;
     }
 
+    /**
+     * Helper method for DFS traversal
+     *
+     * @param node    - current node
+     * @param graph   - adjacency list of graph
+     * @param visited - boolean array tracking visited nodes
+     */
+    private void dfs(int node, Map<Integer, List<Integer>> graph, boolean[] visited) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(node);
+        visited[node] = true;
+
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            for (int neighbor : graph.getOrDefault(current, new ArrayList<>())) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            }
+        }
+    }
 }
